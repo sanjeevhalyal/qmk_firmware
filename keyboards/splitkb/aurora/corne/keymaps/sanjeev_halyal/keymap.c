f89bfa449631d91e80c75f4b5cf7aacd674db2fe
sanjeev Halyal <sanjeevhalyal@gmail.com>
2024-06-17 09:10:05 +0100

    remove inc and add cap words

--- keyboards/splitkb/aurora/corne/keymaps/sanjeev_halyal/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/sanjeev_halyal/keymap.c
@@ -13,18 +13,15 @@ enum layers {
     _THIRD
 };
 
-// #   define RGBLIGHT_EFFECT_SNAKE
 
 enum {
-    TD_ESC_CAPS,
-	TD_SFT_CWRD
+    TD_ESC_CAPS
 };
 
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
-    [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
-	[TD_SFT_CWRD] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT,CW_TOGG)
+    [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS)
 };
 
 
@@ -35,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		  	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
 			 KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,                   						KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
 			//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-			TD(TD_SFT_CWRD), KC_Z, KC_X, KC_C, KC_V, KC_B,                    						KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(TD_ESC_CAPS),
+			KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                    						KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(TD_ESC_CAPS),
 			//|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
 			
 				                    				KC_LGUI, MO(1), KC_SPC,              KC_ENT, MO(2), KC_RALT
